3ae7bb9cecb88b36f15ba0687dcfef13fec23bc7
emaddaem <emaddaem@gmail.com>
2023-06-14 15:49:46 +0100

    mm

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,42 +1,62 @@
 @extends('base')
 @section('content')
-    <main class="login-form">
-        <div class="login-container">
-            <div class="row justify-content-center">
-                <div class="col-md-4">
-                    <div class="card">
-                        <h3 class="card-header text-center">Se connecter</h3>
-                        <div class="card-body">
-                            <form method="POST" action="{{ route('login.custom') }}">
-                                @csrf
-                                <div class="form-group mb-3">
-                                    <input type="text" placeholder="Email" id="email" class="form-control" name="email" required
-                                           autofocus>
-                                    @if (isset($errors) && $errors->has('email'))
-                                        <span class="text-danger">{{ $errors->first('email') }}</span>
-                                    @endif
-                                </div>
-                                <div class="form-group mb-3">
-                                    <input type="password" placeholder="Password" id="password" class="form-control" name="password" required>
-                                    @if (isset($errors) && $errors->has('password'))
-                                        <span class="text-danger">{{ $errors->first('password') }}</span>
-                                    @endif
-                                </div>
-                                <div class="form-group mb-3">
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" name="remember"> Remember Me
-                                        </label>
-                                    </div>
-                                </div>
-                                <div class="d-grid mx-auto">
-                                    <button type="submit" class="btn btn-dark btn-block">Connexion</button>
-                                </div>
-                            </form>
+<div class="container d-flex align-items-center justify-content-center" style="min-height: 100vh">
+    <div class="d-flex flex-column justify-content-between">
+        <div class="row justify-content-center">
+            <div class="col-lg-6 col-md-10">
+                <div class="card card-default mb-0">
+                    <div class="card-header pb-0">
+                        <div class="app-brand w-100 d-flex justify-content-center border-bottom-0">
+                            <a class="w-auto pl-0" href="/index.html">
+                                <!-- <img src="images/logo.png" alt="Mono"> -->
+                                <span class="brand-name text-dark">MONO</span>
+                            </a>
                         </div>
+                    </div>
+                    <div class="card-body px-5 pb-5 pt-0">
+
+                        <h4 class="text-dark mb-6 text-center">Sign in for free</h4>
+
+                        <form method="POST" action="{{ route('login.custom') }}">
+                            @csrf
+                            <div class="row">
+                                <div class="form-group col-md-12 mb-4">
+                                    <input type="email" class="form-control input-lg" name="email" id="email" aria-describedby="emailHelp" placeholder="email">
+                                    @if (isset($errors) && $errors->has('email'))
+                                    <span class="text-danger">{{ $errors->first('email') }}</span>
+                                    @endif
+                                </div>
+                                <div class="form-group col-md-12 ">
+                                    <input type="password" class="form-control input-lg" id="password" name="password" placeholder="Password">
+                                    @if (isset($errors) && $errors->has('password'))
+                                    <span class="text-danger">{{ $errors->first('password') }}</span>
+                                    @endif
+                                </div>
+                                <div class="col-md-12">
+
+                                    <div class="d-flex justify-content-between mb-3">
+
+                                        <div class="custom-control custom-checkbox mr-3 mb-3">
+                                            <input type="checkbox" class="custom-control-input" id="customCheck2">
+                                            <label class="custom-control-label" for="customCheck2">Remember me</label>
+                                        </div>
+
+                                        <a class="text-color" href="#"> Forgot password? </a>
+
+                                    </div>
+
+                                    <button type="submit" class="btn btn-primary btn-pill mb-4">Sign In</button>
+
+                                    <p>Don't have an account yet ?
+                                        <a class="text-blue" href="sign-up.html">Sign Up</a>
+                                    </p>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
-    </main>
+    </div>
+</div>
 @endsection
--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -5,21 +5,48 @@
     <title>@yield('title')</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- MONO -->
+    <link href="https://fonts.googleapis.com/css?family=Karla:400,700|Roboto" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <!-- <script src="{{ asset('assets/plugins/jquery-3.6.0.min.js') }}"></script> -->
+    <!-- GOOGLE FONTS -->
+    <link href="{{ asset('assets/plugins/material/css/materialdesignicons.min.css') }}" rel="stylesheet" />
+
+    <link href="{{ asset('assets/plugins/simplebar/simplebar.css') }}" rel="stylesheet" />
+
+    <!-- PLUGINS CSS STYLE -->
+    <link href="{{ asset('assets/plugins/nprogress/nprogress.css') }}" rel="stylesheet" />
+
+    <link href="{{ asset('assets/plugins/DataTables/DataTables-1.10.18/css/jquery.dataTables.min.css') }}" rel="stylesheet" />
+
+    <link href="{{ asset('assets/plugins/jvectormap/jquery-jvectormap-2.0.3.css') }}" rel="stylesheet" />
+
+    <link href="{{ asset('assets/plugins/daterangepicker/daterangepicker.css') }}" rel="stylesheet" />
+
+    <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+
+    <link href="{{ asset('assets/plugins/toaster/toastr.min.css') }}" rel="stylesheet" />
+
+    <!-- MONO CSS -->
+    <link id="main-css-href" rel="stylesheet" href="{{ asset('assets/css/style.css') }}" />
+
+    <script src="{{ asset('assets/plugins/nprogress/nprogress.js') }}"></script>
+
+    <link rel="stylesheet" href="{{ asset('css/fontawesome-free-6.4.0/css/all.min.css') }}">
+    
+    <!-- MONO -->
+
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"> -->
     <!-- <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}"> -->
     <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="{{ asset('css/fontawesome-free-6.4.0/css/all.min.css') }}">
-    <style>
-        body {
-        background-color: #6b69d6;
-    }
-    </style>
+
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script> -->
+    <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> -->
 </head>
 
-<body>
+<body class="bg-light-gray" id="body">
 
     @include('includes.header')
 
@@ -72,6 +99,48 @@
             document.getElementById('div-changement-mot-de-passe').style.display = 'block';
         }
     </script>
+
+    <script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/simplebar/simplebar.min.js') }}"></script>
+    <script src="https://unpkg.com/hotkeys-js/dist/hotkeys.min.js"></script>
+
+    <script src="{{ asset('assets/plugins/apexcharts/apexcharts.js') }}"></script>
+
+    <script src="{{ asset('assets/plugins/DataTables/DataTables-1.10.18/js/jquery.dataTables.min.js') }}"></script>
+
+    <script src="{{ asset('assets/plugins/jvectormap/jquery-jvectormap-2.0.3.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/jvectormap/jquery-jvectormap-world-mill.js') }}"></script>
+    <script src="{{ asset('assets/plugins/jvectormap/jquery-jvectormap-us-aea.js') }}"></script>
+
+    <script src="{{ asset('assets/plugins/daterangepicker/moment.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/daterangepicker/daterangepicker.js') }}"></script>
+    <script>
+        jQuery(document).ready(function() {
+            jQuery('input[name="dateRange"]').daterangepicker({
+                autoUpdateInput: false,
+                singleDatePicker: true,
+                locale: {
+                    cancelLabel: 'Clear'
+                }
+            });
+            jQuery('input[name="dateRange"]').on('apply.daterangepicker', function(ev, picker) {
+                jQuery(this).val(picker.startDate.format('MM/DD/YYYY'));
+            });
+            jQuery('input[name="dateRange"]').on('cancel.daterangepicker', function(ev, picker) {
+                jQuery(this).val('');
+            });
+        });
+    </script>
+
+    <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+
+    <script src="{{ asset('assets/plugins/toaster/toastr.min.js') }}"></script>
+
+    <script src="{{ asset('assets/js/mono.js') }}"></script>
+    <script src="{{ asset('assets/js/chart.js') }}"></script>
+    <script src="{{ asset('assets/js/map.js') }}"></script>
+    <script src="{{ asset('assets/js/custom.js') }}"></script>
 
 </body>
 
